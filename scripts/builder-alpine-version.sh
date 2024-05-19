a04013eaeb9f23aaef76a3365e74557dc6380400
PhilLavoie <PhilLavoie@users.noreply.github.com>
2024-05-19 07:53:23 -0400

    Migrate to infra-blocks

--- scripts/builder-alpine-version.sh
+++ scripts/builder-alpine-version.sh
@@ -4,7 +4,7 @@ set -e
 
 BUILDER_TAG=${BUILDER_TAG:?"missing BUILDER_TAG environment variable!"}
 
-builder_image="public.ecr.aws/infrastructure-blocks/docker-rust-builder-base:${BUILDER_TAG}"
+builder_image="public.ecr.aws/infra-blocks/docker-rust-builder-base:${BUILDER_TAG}"
 alpine_version=$(docker run --rm -q "${builder_image}" cat /etc/alpine-release)
 # Only keep the major and minor version.
 echo "${alpine_version%.*}"
